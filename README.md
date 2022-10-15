c8a86b12e6003d965de83f7eb24c62016480dc87
Marcio Bispo <91513221+EngMarcioBispo@users.noreply.github.com>
2022-10-14 22:47:49 -0300

    Adicionar arquivos de projeto.

---
+++ README.md
@@ -0,0 +1,7 @@
+Este é um teste de avaliação para a posição de desenvolvedor.
+
+Existem 2 testes:
+
+1: Imagine que temos um arquivo de legenda (.srt) fora de sincronia. O programa deve ser capaz de deslocar todo o arquivo de origem no intervalo de tempo determinado.
+
+2: Dada uma lista predefinida de links, o programa deve ser capaz de calcular os caminhos possíveis entre um ponto e outro.
---
+++ SubtitleTimeshift/Shifter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubtitleTimeshift
+{
+    public class Shifter
+    {
+        async static public Task Shift(Stream input, Stream output, TimeSpan timeSpan, Encoding encoding, int bufferSize = 1024, bool leaveOpen = false)
+        {
+            try {
+
+                using (var read = new StreamReader(input, encoding, false, bufferSize, leaveOpen))
+                using (var write = new StreamWriter(output, encoding, bufferSize, leaveOpen))
+                {
+                    string row;
+                    var spacer = " --> ";
+                    while ((row = await read.ReadLineAsync()) != null)
+                    {
+                        var outputLine = row;
+                        var stamps = row.Split(new[] {spacer}, StringSplitOptions.RemoveEmptyEntries);
+                        if (stamps.Length == 2)
+                        {
+                            var start = shiftStamp(stamps[0], timeSpan);
+                            var end = shiftStamp(stamps[1], timeSpan);
+                            outputLine = $"{start}{spacer}{end}";
+                        }
+
+                        await write.WriteLineAsync(outputLine);
+                    }
+                }
+            }
+            catch (Exception e) {                
+                throw e;
+            }
+        }
+
+        static string shiftStamp(string stampText, TimeSpan timeSpan)
+        {
+            var numbers = stampText.Split(new[] { ':', ',', '.', '|' })
+                                .Select(s => int.Parse(s))
+                                .ToArray();
+
+            var result = new TimeSpan(0, numbers[0], numbers[1], numbers[2], numbers[3])
+                                .Add(timeSpan)
+                                .ToString(@"hh\:mm\:ss\.fff");
+            return result;
+        }
+    }
+}
